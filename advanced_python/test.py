e660ca42d79097004e580f3add8a8f34e046815f
Fan Zhou <107796731+louisefz@users.noreply.github.com>
2022-06-20 01:13:58 +0200

    Delete advanced_python directory

--- advanced_python/test.py
+++
@@ -1,8 +0,0 @@
-import umap
-umap_data = umap.UMAP(n_neighbors=5, min_dist=0.3, n_components=3).fit_transform(df[feat_cols][:6000].values)
-
-plt.figure(figsize=(12,8))
-plt.title('Decomposition using UMAP')
-plt.scatter(umap_data[:,0], umap_data[:,1])
-plt.scatter(umap_data[:,1], umap_data[:,2])
-plt.scatter(umap_data[:,2], umap_data[:,0])
